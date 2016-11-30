3f7568ff57d4b960dea2ee8340a2356b4a852938
Junichi HAYASHI <junchan.kcct@gmail.com>
2016-11-30 11:30:03 +0900

    solve ABC#005 A

---
+++ ABC/005/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+using namespace std;
+
+const int INF = 1e8;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << y/x << endl;
+
+    return 0;
+}
