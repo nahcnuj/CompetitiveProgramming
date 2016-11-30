05b417862674762efeacad04c5f0081aad9d2314
Junichi HAYASHI <junchan.kcct@gmail.com>
2016-11-30 10:57:17 +0900

    solve ABC#004 A

---
+++ ABC/004/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+using namespace std;
+    
+const int INF = 1e8;
+    
+int main() {
+    int N;
+    cin >> N;
+    cout << 2 * N << endl;
+    return 0;
+}
