ed5022f65a9e0456dd43a3a153cb22e15fbec39f
Junichi Hayashi <j-hayasi@ist.osaka-u.ac.jp>
2019-05-19 01:20:13 +0900

    Solve ABC 004 D by the editorial's solution

--- ABC/004/d.cpp
+++ ABC/004/d.cpp
@@ -1,60 +1,25 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cstdio>
-#include <algorithm>
-#include <functional>
-#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e8;
-
-// x個の同色のマーブルを左右一方の側で1個以下にする
-int f(int x) {
-    if (x <= 0) return 0;
-
-    return x * (x-1) / 2;
-}
-
-// x個の同色のマーブルを左右両方で1個以下にする
-int g(int x) {
-    return x/2 + f(x/2) + f((x+1)/2);
-}
+const int INF = 1<<30;
 
 int main() {
     int R, G, B;
     cin >> R >> G >> B;
 
-    if (G <= 199) {
-        // all green marbles go in a range of [-99, 99]
-        cout << g(R) + g(G) + g(B) << endl;
+    int ans = INF;
+
+    for (int lG = -1000; lG <= 1000; ++lG) {
+        int lR = min(-100 - R/2, lG - R);
+        int lB = max( 100 - B/2, lG + G);
+        int s = 0;
+        for (int i = lR; i < lR + R; ++i) s += abs(-100 - i);
+        for (int i = lG; i < lG + G; ++i) s += abs(   0 - i);
+        for (int i = lB; i < lB + B; ++i) s += abs( 100 - i);
+        ans = min(ans, s);
     }
-    else {
-        int ans = INF;
-        for (int k = 0; k <= G; ++k) {
-            int rt, gt, bt;
 
-            int rightR = 99 - (G - k);
-            if (rightR > 0) {
-                rt = rightR + f(rightR) + f(R - rightR);
-            }
-            else {
-                rt = (G - 99 - k) * R + f(R);
-            }
-
-            gt = f(k) + (G - k) + f(G - k);
-
-            int leftB = 99 - k;
-            if (leftB > 0) {
-                bt = leftB + f(leftB) + f(B - leftB);
-            }
-            else {
-                bt = (k - 100) * B + f(B);
-            }
-            ans = min(ans, rt + gt + bt);
-        }
-        cout << ans << endl;
-    }
+    cout << ans << endl;
 
     return 0;
 }
