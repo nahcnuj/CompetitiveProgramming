6af51c37c79b1819c247930bc69ccd4437471a13
Junichi Hayashi <junchan.kcct@gmail.com>
2021-09-06 12:26:31 +0900

    indent

--- rcl-contest-2021-long/bin/generate-csv-for-graph.bash
+++ rcl-contest-2021-long/bin/generate-csv-for-graph.bash
@@ -8,13 +8,32 @@ filename_pattern=${1:-scores_*.csv}
 seeds=$(echo -n {0..9} | tr ' ' ',')
 
 read -d '' calc_avg <<'EOS' || true
-    BEGIN { mi=999999999; ma=-999999999 }
-    { cnt++; sum+=$2; mi=mi>$2?$2:mi; ma=ma<$2?$2:ma; printf "%d,", $2 }
-    END { avg=sum/cnt; printf "%.2f,%.2f,%.2f,", mi, avg, ma }
+    BEGIN {
+        mi= 999999999;
+        ma=-999999999;
+    }
+    {
+        cnt++;
+        sum+=$2;
+        mi=mi>$2?$2:mi;
+        ma=ma<$2?$2:ma;
+        printf "%d,", $2;
+    }
+    END {
+        avg=sum/cnt;
+        printf "%.2f,%.2f,%.2f,", mi, avg, ma;
+    }
 EOS
 
 read -d '' calc_dev <<'EOS' || true
-    { printf $0; for (i=2; i<NF-3; i++) stdev+=($i-$13)*($i-$13)/(NF-4-1); sterr=sqrt(stdev); printf "%.2f,%.2f,%.2f,", sterr, $13-sterr, $13+sterr }
+    {
+        printf $0;
+        for (i=2; i<NF-3; i++) {
+            stdev+=($i-$13)*($i-$13)/(NF-4-1);
+        }
+        sterr=sqrt(stdev);
+        printf "%.2f,%.2f,%.2f,", sterr, $13-sterr, $13+sterr;
+    }
 EOS
 
 echo parameter,${seeds},min,avg,max,σ,μ-σ,μ+σ,
